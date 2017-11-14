66ff8e7f62c4fdae7df8dd9df1842d36ca4c8e00
Jake Greenberg-Bell <jacobgreenbergbell@students.berkeley.net>
2017-11-14 11:38:42 -0800

    third draft

--- script.js
+++ script.js
@@ -1,73 +1,76 @@
 function setUpVars() {
-    easyWords = [ "dude",    "truck",   "soccer", "purple", "adult", "album", "alphabet", "apple", "apple", +
-        "balloon", "banana",   "bathtub", "bottle",  "button", "clown",  "clown",  "dress",  "elephant", +
-        "eraser", "family", "garden",   "hammer",   "passport", "pebble",  "school"];
-    mediumWords = ["angel", "flower", "absurd","weird", "jazz","airplane", "airport", "backpack", "bathroom", +
-        "chocolate","diamond", "festival", "freeway", "fungus","knife", "guitar", "kitchen","library", "magnet", +
-        "maze","necklace","television", "toilet", "torpedo", "parachute"];
-    hardWords = ["awesome", "ridiculous", "fluorescent", "tranquilizer", "disgusting", "barbecue", "crystal", +
-        "cappuccino","butterfly", "electricity","explosive","gemstone", "gloves","kaleidoscope","meteor", +
-        "microscope", "milkshake", "monster", "mosquito","pendulum", "pyramid", "satellite", "signature", +
-        "skeleton", "surveyor", "vacuum", "vampire", "vulture"];
+    easyWords = [ "DUDE",    "TRUCK",   "SOCCER", "PURPLE", "ADULT", "ALBUM", "ALPHABET", "APPLE", "APPLE", +
+        "BALLOON", "BANANA",   "BATHTUB", "BOTTLE",  "BUTTON", "CLOWN",  "BOMB",  "DRESS",  "ELEPHANT", +
+        "ERASER", "FAMILY", "GARDEN",   "HAMMER",   "PASSPORT", "PEBBLE",  "SCHOOL"];
+    mediumWords = ["ANGEL", "FLOWER", "ABSURD","WEIRD", "JAZZ","AIRPLANE", "AIRPORT", "BACKPACK", "BATHROOM", +
+        "CHOCOLATE","DIAMOND", "FESTIVAL", "FREEWAY", "FUNGUS","KNIFE", "GUITAR", "KITCHEN","LIBRARY", "MAGNET", +
+        "MAZE","NECKLACE","TELEVISION", "TOILET", "TORPEDO", "PARACHUTE"];
+    hardWords = ["AWESOME", "RIDICULIOUS", "FLUORESCENT", "TRANQUILIZER", "DISGUSTING", "BARBECUE", "CRYSTAL", +
+        "CAPPUCCINO","BUTTERFLY", "ELECTRICITY","EXPLOSIVE","GEMSTONE", "GLOVES","KALEIDOSCOPE","METEOR", +
+        "MICROSCOPE", "MILKSHAKE", "MONSTER", "MOSQUITO","PENDULUM", "PYRAMID", "SATELLITE", "SIGNATURE", +
+        "SKELETON", "SURVEYOR", "VACUUM", "VAMPIRE", "VULTURE"];
     wordPlaceInArray = Math.floor(Math.random() * 24);
+    output = [];
+    word = [];
+    letterOptions = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R",
+    "S", "T", "U", "V", "W", "X", "Y", "Z"];
+    incorrectGuesses = [];
 }
 
 function findWord(level){
-    word = "" ;
-    var underscores = "This is the word: ";
+    var underscores = "";
     switch (level) {
         case "easyWords":
             word = easyWords[wordPlaceInArray];
-            for(var i = 0; i < word.length; i++){
-                underscores += "____   ";
-            }
-            document.getElementById("underscores").innerHTML = underscores;
-            break;
+
+
         case "mediumWords":
             word = mediumWords[wordPlaceInArray];
-            for(var i = 0; i < word.length; i++){
-                underscores += "____   ";
-            }
-            document.getElementById("underscores").innerHTML = underscores;
-            break;
+
+
         case "hardWords":
             word = hardWords[wordPlaceInArray];
-            for(var i = 0; i < word.length; i++){
-                underscores += "____   ";
-            }
-            document.getElementById("underscores").innerHTML = underscores;
     }
+    for(var i = 0; i < word.length; i++) {
+        output.push("_ ");
+    }
+    var outputFinal = output.join();
+    document.getElementById("underscores").innerHTML = outputFinal.replace(/,/g, "");
     console.log(word);
 }
 
-function makeAGuess(){
+function makeAGuess() {
     var letterGuess = document.getElementById("selectLetter").value;
     console.log(letterGuess);
-    var isItThere;
-    for(var i = 0; i < word.length; i ++){
-        if(letterGuess == word[i]){
-            isItThere = true;
-            popLetter(i,letterGuess);
-            document.getElementById("underscores").innerHTML[i] = letterGuess;
-
-            break;
-        } else{
-            isItThere = false;
+    var guessArray = [];
+    removeLetter(letterGuess);
+    if(word.includes(letterGuess) != true){
+        incorrectGuesses.push(letterGuess);
+        console.log(incorrectGuesses);
+        document.getElementById("incorrectGuesses").innerHTML = "INCORRECT LETTERS: " + incorrectGuesses + "  ";
+    }
+    for (var i = 0; i < word.length; i++) {
+        if (letterGuess == word[i]) {
+            guessArray[i] = true;
+        } else {
+            guessArray[i] = false;
         }
 
     }
-
+    pushToHTML(guessArray, letterGuess);
 }
 
-function popLetter(location, letter){
-    var underscores = "This is the word: ";
-    for(var i = 0; i < word.length; i++){
-        if (i == location){
-            underscores += "  " + letter + "  ";
-        }else{
-            underscores += "____   ";
-        }
+function pushToHTML(arrayOfGuesses, guess){
+    for(var i = 0; i < arrayOfGuesses.length; i++){
+            if(arrayOfGuesses[i]){
+                output[i] = guess;
+            }
     }
-    document.getElementById("underscores").innerHTML = underscores;
+    var outputFinal = output.join();
+    document.getElementById("underscores").innerHTML = outputFinal.replace(/,/g, "");
+}
 
+function removeLetter(letter){
+    var letterBox = document.getElementById("selectLetter");
+    letterBox.remove(letterBox.selectedIndex);
 }
